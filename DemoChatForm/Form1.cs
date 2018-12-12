1a6666d2f7d05586b11e166a0c721097bf7cc34f
ThomasAndreatta <thomas.andreatta2000@gmail.com>
2018-12-12 10:59:04 +0100

    1versione invio struct, da sistemare l'aggiornamento struct ricevente e la lista utenti connessi

--- DemoChatForm/Form1.cs
+++ DemoChatForm/Form1.cs
@@ -16,12 +16,11 @@ using System.Collections.Generic;
 
 namespace DemoChatForm
 {
-# region COSE DA FARE
-   
-        //inserire la steath dalle impo assieme alla lingua(cercare di pescare la roba da un file e non da if dai
-        //cripto alla chat con chiave inserita nel form iniziale
-        //invio del numero degli utenti quando qualcuno logga e salvare il numero maggiore, mandare anche lo struct "numero|nome|ip" e aggiornare in base all'ultimo
-        #endregion
+    #region COSE DA FARE
+
+    //inserire la steath dalle impo assieme alla lingua(cercare di pescare la roba da un file e non da if dai
+    //ogni volta che qualcuno entra ed esce c'è da aggiornare lo struct listautenti e mostrarlo al click dell'immagine in un piccolo form con una tabella indirizzi e username.
+    #endregion
     public partial class Form1 : Form
     {
         #region HotKey
@@ -33,7 +32,7 @@ namespace DemoChatForm
         [DllImport("user32.dll")]
         public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
         #endregion
-        
+
         #region variabili 
         private struct Indirizzi
         {
@@ -47,19 +46,19 @@ namespace DemoChatForm
         private Thread th1;
         private bool minimizato = false;
         private bool continua = true, Qr = false;
-        private Icon MessNonLetto = new Icon(Resources.Notifica2, new Size(50, 50));
+        private Icon MessNonLetto = new Icon(Resources.Notifica2, new Size(51, 50));
         private Icon Logo = new Icon(Resources.logo, new Size(50, 50));
         private QrCode code;
         const int mostra = 2, nascondi = 1;
         private KeyboardHook hook = new KeyboardHook();
-        
+
         #endregion
 
         #region Eventi Del Form
         public Form1()
-        {           
+        {
             InitializeComponent();
-           
+
             listaUtenti.nomi = new List<string>();
             listaUtenti.ip = new List<IPAddress>();
             Username coso = new Username();
@@ -77,32 +76,32 @@ namespace DemoChatForm
                 }
                 else
                 {
-                    tx.CambiaUsername( coso.username);
+                    tx.CambiaUsername(coso.username);
                     rx = new riceviUDP(9050);
                     th1 = new Thread(Ascolta);
                     th1.Start();
                     tx.Invia($"                                   >>>({tx.Ip}){tx.Username.ToUpper()} ENTRA NELLA CHAT<<<", 1);
-                }               
+                }
             }
-            
+
             #region HOTKEY           
             RegisterHotKey(this.Handle, nascondi, 6, (int)Keys.Q);
             RegisterHotKey(this.Handle, mostra, 6, (int)Keys.W);
             #endregion
 
-        }        
+        }
         protected override void WndProc(ref Message m)
         {
             if (m.Msg == 0x0312 && m.WParam.ToInt32() == nascondi)
             {
-                this.WindowState = FormWindowState.Minimized;                
+                this.WindowState = FormWindowState.Minimized;
             }
             else if (m.Msg == 0x0312 && m.WParam.ToInt32() == mostra)
             {
                 this.WindowState = FormWindowState.Normal;
             }
             base.WndProc(ref m);
-        }       
+        }
         private void BtnInvia_Click(object sender, EventArgs e)
         {
             if (txtMsg.Text != "")
@@ -133,7 +132,7 @@ namespace DemoChatForm
                 }
 
             }
-        }  
+        }
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             tx.Invia($"                                   >>>{tx.Username.ToUpper()} ESCE DALLA CHAT<<<", 1);
@@ -202,45 +201,79 @@ namespace DemoChatForm
             string msg;
             while (continua)
             {
-                msg = rx.ricevi();                
-                
-                if (this.WindowState == FormWindowState.Minimized && !msg.Contains(tx.Username.ToUpper()+" ENTRA NELLA CHAT"))
+                msg = rx.ricevi();
+
+                if (this.WindowState == FormWindowState.Minimized && !msg.Contains(tx.Username.ToUpper() + " ENTRA NELLA CHAT"))
                 {
                     this.Icon = MessNonLetto;
                     minimizato = !minimizato;
-                    lstBoxMsg.Items.Add(msg.Split('|')[2]);
                 }
 
-                if (msg.Contains("ENTRA NELLA CHAT"))
+                #region ingressi e dati
+                if (msg.Contains("ENTRA NELLA CHAT"))//
                 {
-                    lstBoxMsg.Items.Add(msg.Split('|')[2]);
-                    listaUtenti.nomi.Add(msg.Split('|')[1]);
                     listaUtenti.ip.Add(IPAddress.Parse(msg.Split('|')[0]));
+                    listaUtenti.nomi.Add(msg.Split('|')[1]);
                     CountPersone++;
-                    lblPersone.Text = ""+CountPersone;
-                }
-                 else if (msg.Contains("ESCE DALLA CHAT"))
-                {
+                    aggiornaLbl();
                     lstBoxMsg.Items.Add(msg.Split('|')[2]);
-                    listaUtenti.nomi.RemoveAll((x) => x == msg.Split('|')[1]);
-                    listaUtenti.ip.RemoveAll((x) => x == IPAddress.Parse(msg.Split('|')[0]));
-                    CountPersone--;
-                    lblPersone.Text = "" + CountPersone;
-                }else
-                {
-                    lstBoxMsg.Items.Add(msg);
+                    tx.Invia("struct§ù§|" + listaUtenti.nomi.Count + "|" + ritornaIp() + "|" + ritornaNomi());
                 }
-
+                if (msg.Contains("ESCE DALLA CHAT"))//ip|nome|msg
+                {
+                    listaUtenti.ip.Remove(IPAddress.Parse(msg.Split('|')[0]));
+                    listaUtenti.nomi.Remove(msg.Split('|')[1]);
+                    lstBoxMsg.Items.Add(msg.Split('|')[2]);
+                    CountPersone--;
+                    aggiornaLbl();
+                }
+                if (msg.Contains("struct§ù§") && Convert.ToInt32(msg.Split('|')[1]) > CountPersone) //struct§ù§|numero|ip|username
+                {
+                    leggiIp(msg.Split('|')[2], msg.Split('|')[3], Convert.ToInt32(msg.Split('|')[1]));
+                }
+                #endregion
             }
         }
+        private void leggiIp(string listoneIp, string listoneNomi, int numero)
+        {
+            listaUtenti.ip.RemoveAll((x) => x != null);
+            listaUtenti.nomi.RemoveAll((x) => x != null);
+            for (int i = 0; i <= numero; i++)
+            {
+                listaUtenti.ip.Add(IPAddress.Parse(listoneIp.Split('-')[i]));
+                listaUtenti.nomi.Add(listoneIp.Split('-')[i]);
+            }
+            aggiornaLbl();
+        }
+        private string ritornaIp()
+        {
+            string listaIp = "";
+            foreach (var item in listaUtenti.ip)
+            {
+                listaIp += item + "-";
+            }
+            return listaIp;
+        }
+        private string ritornaNomi()
+        {
+            string coso = "";
+            foreach (var item in listaUtenti.nomi)
+            {
+                coso += item + "-";
+            }
+            return coso;
+        }
         #endregion
-
+        private void aggiornaLbl()
+        {
+            lblPersone.Text = "" + listaUtenti.nomi.Count;
+        }
         #region Icone
         private void CambiaUsernameIcon_Click(object sender, EventArgs e)
         {
             Username newUsername = new Username();
             if (newUsername.ShowDialog() == DialogResult.OK)
-            {                
+            {
                 tx.Invia($"                                   >>>{tx.Username.ToUpper()} HA CAMBIATO NOME IN {newUsername.username}<<<", 1);
                 tx.CambiaUsername(newUsername.username);
             }
@@ -248,7 +281,7 @@ namespace DemoChatForm
         private void QrIcon_MousoOver(object sender, EventArgs e)
         {
             Qr = true;
-            code = new QrCode(System.Windows.Forms.Cursor.Position.X + 10, System.Windows.Forms.Cursor.Position.Y+10);
+            code = new QrCode(System.Windows.Forms.Cursor.Position.X + 10, System.Windows.Forms.Cursor.Position.Y + 10);
             code.Show();
         }
         private void QrIcon_MouseLeave(object sender, EventArgs e)
@@ -276,11 +309,11 @@ namespace DemoChatForm
         private void Salva()
         {
             SaveFileDialog save = new SaveFileDialog();
-            
+
             save.FileName = "Chat" + " " + DateTime.Now.ToShortDateString().Replace('/', '-');
             save.DefaultExt = "txt";
             save.Title = "Save Chat";
-            
+
             save.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
             if (save.ShowDialog() == DialogResult.OK)
             {
@@ -294,16 +327,22 @@ namespace DemoChatForm
                 }
                 MessageBox.Show("Chat saved as: " + save.FileName);
             }
-        }      
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(ritornaIp());
+        }
+
         private void ImpostazioniIcon_Click(object sender, EventArgs e)
         {
-            Point a = new Point(System.Windows.Forms.Cursor.Position.X + 10, System.Windows.Forms.Cursor.Position.Y -40);
-           Notifica not = new Notifica("Ti ho detto che non funziono.", 5, a);
+            Point a = new Point(System.Windows.Forms.Cursor.Position.X + 10, System.Windows.Forms.Cursor.Position.Y - 40);
+            Notifica not = new Notifica("Ti ho detto che non funziono.", 5, a);
             not.Show();
-        }     
+        }
         private void InfoIcon_Click(object sender, EventArgs e)
         {
-          
+
             Info info = new Info();
             info.Show();
         }
